a37e51dc50502443e02092cf9e28ae9f04dce442
tsanchez007 <info.tauliosanchez@gmail.com>
2020-03-23 23:20:11 -0400

    Formulario 02
    Formulario 2.0

--- enviar.php
+++ enviar.php
@@ -1,34 +1,13 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-	<title>Formulario de contacto</title>
-</head>
+<?php 
 
-<body>
-	<center>
-	<?php 
-$myemail = 'info.tauliosanchez@gmail.com';
-$name = $_POST['nombre'];
-$email = $_POST['email'];
-$message = $_POST['mensaje'];
+	$destino= "info.tauliosanchez@gmail.com";
+	$nombre = $_POST["nombre"];
+	$correo = $_POST["correo"];
+	$telefono = $_POST["telefono"];
+	$mensaje = $_POST["mensaje"];
+	$contenido = "nombre: " . $nombre . "\ncorreo: " . $correo . "\ntelefono: " . $telefono . "\nmensaje: " . $mensaje;
+	mail($destino,"Contacto",$contenido);
+	header("location:index.html");
 
-$to = $myemail;
-$email_subject = "Nuevo mensaje: $subject";
-$email_body = "Haz recibido un nuevo mensaje. \n Nombre: $name \n Correo: $email \n Mensaje: \n $message";
-$headers = "From: $email";
 
-mail($to, $email_subject, $email_body, $headers);
-echo "El mensaje se ha enviado correctamente";
-?>
-
-		<form method="post" action="enviar.php">
-			<input type="text" name="nombre">
-			<textarea type="email" name="mensaje" placeholder="mensaje"></textarea>
-			<input type="submit" value="enviar">
-		</form>
-	</center>
-
-                    
-</body>
-</html>
+ ?>
