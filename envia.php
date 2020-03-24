ab1ee8503c23e390c98b104905f33eb21135b5e0
tsanchez007 <info.tauliosanchez@gmail.com>
2020-03-23 23:33:16 -0400

    Nuevo 03
    Nuevo 03

--- envia.php
+++ envia.php
@@ -1,6 +1,6 @@
 ﻿<?php
 $remitente = $_POST['email'];
-$destinatario = 'mail@dominio.com'; // en esta línea va el mail del destinatario.
+$destinatario = 'info.tauliosanchez@gmail.com'; // en esta línea va el mail del destinatario.
 $asunto = 'Consulta'; // acá se puede modificar el asunto del mail
 if (!$_POST){
 ?>
